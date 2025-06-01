983f7e642f62b64351b93226ab1147098834cc67
John Mylchreest <jmylchreest@gmail.com>
2025-06-01 08:50:30 +0100

    Revert "ci: test out using cross w/ matrix builders"
    This reverts commit 1bf735ff8984b0940963df07b9b35154f44556f5.
    
    Revert "ci: try without goreleaser and using cross"
    
    This reverts commit bc19ef0b397977e528c0e6bfcb29ecae35a13132.
    
    Revert "ci: overide CC/CXX/AR"
    
    This reverts commit e277dba9323d05b587ea8eb25aa17d490acea1b7.

--- src/mcp_server_kagisearch.rs
+++ src/mcp_server_kagisearch.rs
@@ -45,12 +45,12 @@ impl KagiModelContextExtension {
             arch = match arch {
                 zed::Architecture::Aarch64 => "arm64",
                 zed::Architecture::X86 => "i386",
-                zed::Architecture::X8664 => "x64",
+                zed::Architecture::X8664 => "x86_64",
             },
             os = match platform {
-                zed::Os::Mac => "darwin",
-                zed::Os::Linux => "linux",
-                zed::Os::Windows => "windows",
+                zed::Os::Mac => "Darwin",
+                zed::Os::Linux => "Linux",
+                zed::Os::Windows => "Windows",
             },
             ext = match platform {
                 zed::Os::Mac | zed::Os::Linux => "tar.gz",
